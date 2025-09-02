feaec425a9c2457162a2dc8e02d453d5c0e0ea67
ConorColwell97 <conormcolwell@gmail.com>
2025-09-03 00:11:27 +0100

    Cross Orign included for testing

--- src/main/java/com/example/MS_Backend/controllers/UserController.java
+++ src/main/java/com/example/MS_Backend/controllers/UserController.java
@@ -3,10 +3,12 @@ package com.example.MS_Backend.controllers;
 import com.example.MS_Backend.models.User;
 import com.example.MS_Backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class UserController {
 
